15c0b82a102864ba78e80dcc8d516cf29cf2c185
brameldering <bram.eldering@gmail.com>
2023-04-20 12:02:45 +0200

    package.json updates

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders two button app", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Clicked/i);
-    expect(linkElement).toBeInTheDocument();
+  render(<App />);
+  const linkElement = screen.getByText(/Clicked/i);
+  expect(linkElement).toBeInTheDocument();
 });
